628bf6284309be56f5c040e180d56f25e36568c6
Mitia-j <dimazmud@gmail.com>
2025-03-19 22:04:24 +0200

    2

--- lp3.asm
+++ lp3.asm
@@ -3,7 +3,8 @@ data segment
     msg db " sot. tys. jitelei$"
     msg21 db "Strana: $"
     msg22 db "Naselenie: $"
-    new_line db 0dh,0ah,'$'
+    new_line db 0dh,0ah,'$'  
+    text db "Vvedite colicestvo stran: $"
     text1 db "Vvedite 1, chtoby vvesti dannye vruchnuiu$"
     text2 db "Vvedite 2, chtoby vvesti predopredelennye dannye$"
     text3 db "Vvedite 3, chtoby otobraziti ishodnye dannye$"
@@ -20,7 +21,7 @@ data segment
     s8 db "France"
     s9 db "Germany"
     s10 db "Switzerland"
-    nas1 dw 23,' ', 377,' ',1438,' ',91,' ',3401,' ',55,' ',1297,' ',682,' ',832,' ',88,' '
+    nas1 dw 23, ' ', 377, ' ', 1438,' ', 91,' ', 3401, ' ',55,' ',1297,' ',682,' ',832,' ',88,' '
 ends
 stack segment
     dw   128  dup(0)
@@ -52,98 +53,124 @@ code segment
         popa  
         ret  
     endp           
+    
+menu1 proc
+    lea dx, text
+    mov ah, 9
+    int 21h
+;vvod colicestva stran    
+    mov ah, 01h
+    int 21h
+    cmp al, '1'
+    je m
+    sub al, 48
+    xor ah,ah  
+    mov cx, ax
+    jmp cicl1
+m:  
+    mov ah, 01h
+    int 21h
+    cmp al,13
+    je cicl1  
+    mov cx,10
+;vvod strany
+cicl1:
+    lea dx,new_line
+    mov ah, 9
+    int 21h   
+    lea dx, msg21
+    mov ah, 9
+    int 21h
+c1:
+    mov ah, 01h
+    int 21h
+    cmp ax,13
+    je c2
+    mov strany[si], al
+    inc si
+    jmp c1
+;vvod naselenia    
+c2:  
+    lea dx,new_line
+    mov ah, 9
+    int 21h
+    
+    lea dx,msg22
+    mov ah, 9
+    int 21h
+    
+zanovo:
+    mov ah, 01h
+    int 21h
+    cmp al,13
+    je end
+    
+    mov bl, al
+    jne zanovo
+    
+    loop cicl1 
+end:            
+    ret                 
+endp
 
-    menu1 proc
-        mov cx, 10
-        repeating:
-            
-        loop repeating
-    cicl1:   
-        lea dx, msg21
-        mov ah, 9
-        int 21h
-    c1:
-        mov ah, 01h
-        int 21h
-        cmp ax, 21h
-        je end
-        mov strany[si], ax
-        inc si
-        jmp c1
-        
-        lea dx,new_line
-        mov ah, 9
-        int 21h   
-        lea dx,msg22
-        mov ah, 9
-        int 21h
-    c2:
-        lea dx,msg22
-        mov ah, 9
-        int 21h
-        
-    end:            
-        ret                 
-    endp
-
-    menu2 proc
-        lea si, s1
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s2
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s3
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s4
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s5
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s6
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s7
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s8
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s9
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, s10
-        lea di, strany
-        mov cx, 20
-        rep movsb
-        
-        lea si, nas1
-        lea di, nasel
-        mov cx, 20
-        rep movsw
-        
-        ret
-    endp
+menu2 proc
+    lea si, s1
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s2
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s3
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s4
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s5
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s6
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s7
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s8
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s9
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, s10
+    lea di, strany
+    mov cx, 20
+    rep movsb
+    
+    lea si, nas1
+    lea di, nasel
+    mov cx, 20
+    rep movsw
+     
+    ret
+endp
 
 start:
     mov ax, data
@@ -179,16 +206,14 @@ start:
     mov ah, 9
     int 21h 
     
-    ;mov ah, 1
-    ;int 21h
+   ; mov ah, 1
+   ; int 21h
+   ; cmp al, '1'
+   ; je m1
     
-    xor ax,ax
+m1:
+    call menu1
     
-    call menu2
-    xor ax,ax
-    lea dx, strany
-    mov ah,9
-    int 21h
     
     mov ax, 4c00h
     int 21h    
